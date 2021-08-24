756497ffa2965d3f5dac89bf35380f0a2a9b8909
sdk-team <sdk-team@alibabacloud.com>
2021-08-24 12:38:22 +0000

    GetHotspotTag Add Params .

--- tdsr-20200101/src/Models/GetHotspotTagResponseBody.php
+++ tdsr-20200101/src/Models/GetHotspotTagResponseBody.php
@@ -9,43 +9,35 @@ use AlibabaCloud\Tea\Model;
 class GetHotspotTagResponseBody extends Model
 {
     /**
-     * @description 请求ID，与入参requestId对应
-     *
      * @var string
      */
     public $requestId;
 
     /**
-     * @description 返回码
-     *
-     * @var int
-     */
-    public $code;
-
-    /**
-     * @description 是否请求成功
-     *
-     * @var bool
-     */
-    public $success;
-
-    /**
-     * @description 错误消息
-     *
      * @var string
      */
-    public $message;
+    public $objectString;
 
     /**
      * @var string
      */
     public $data;
+
+    /**
+     * @var string
+     */
+    public $errMessage;
+
+    /**
+     * @var bool
+     */
+    public $success;
     protected $_name = [
-        'requestId' => 'RequestId',
-        'code'      => 'Code',
-        'success'   => 'Success',
-        'message'   => 'Message',
-        'data'      => 'Data',
+        'requestId'    => 'RequestId',
+        'objectString' => 'ObjectString',
+        'data'         => 'Data',
+        'errMessage'   => 'ErrMessage',
+        'success'      => 'Success',
     ];
 
     public function validate()
@@ -58,17 +50,17 @@ class GetHotspotTagResponseBody extends Model
         if (null !== $this->requestId) {
             $res['RequestId'] = $this->requestId;
         }
-        if (null !== $this->code) {
-            $res['Code'] = $this->code;
-        }
-        if (null !== $this->success) {
-            $res['Success'] = $this->success;
-        }
-        if (null !== $this->message) {
-            $res['Message'] = $this->message;
+        if (null !== $this->objectString) {
+            $res['ObjectString'] = $this->objectString;
         }
         if (null !== $this->data) {
             $res['Data'] = $this->data;
+        }
+        if (null !== $this->errMessage) {
+            $res['ErrMessage'] = $this->errMessage;
+        }
+        if (null !== $this->success) {
+            $res['Success'] = $this->success;
         }
 
         return $res;
@@ -85,17 +77,17 @@ class GetHotspotTagResponseBody extends Model
         if (isset($map['RequestId'])) {
             $model->requestId = $map['RequestId'];
         }
-        if (isset($map['Code'])) {
-            $model->code = $map['Code'];
-        }
-        if (isset($map['Success'])) {
-            $model->success = $map['Success'];
-        }
-        if (isset($map['Message'])) {
-            $model->message = $map['Message'];
+        if (isset($map['ObjectString'])) {
+            $model->objectString = $map['ObjectString'];
         }
         if (isset($map['Data'])) {
             $model->data = $map['Data'];
+        }
+        if (isset($map['ErrMessage'])) {
+            $model->errMessage = $map['ErrMessage'];
+        }
+        if (isset($map['Success'])) {
+            $model->success = $map['Success'];
         }
 
         return $model;
