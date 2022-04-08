9da69252c21fe6a75874583432f647a47f7f3b1a
sdk-team <sdk-team@alibabacloud.com>
2022-04-08 13:41:42 +0000

    Release FeedbackSession.

--- imageprocess-20200320/src/Models/FeedbackSessionResponseBody.php
+++ imageprocess-20200320/src/Models/FeedbackSessionResponseBody.php
@@ -4,16 +4,10 @@
 
 namespace AlibabaCloud\SDK\Imageprocess\V20200320\Models;
 
-use AlibabaCloud\SDK\Imageprocess\V20200320\Models\FeedbackSessionResponseBody\data;
 use AlibabaCloud\Tea\Model;
 
 class FeedbackSessionResponseBody extends Model
 {
-    /**
-     * @var data
-     */
-    public $data;
-
     /**
      * @description Id of the request
      *
@@ -21,7 +15,6 @@ class FeedbackSessionResponseBody extends Model
      */
     public $requestId;
     protected $_name = [
-        'data'      => 'Data',
         'requestId' => 'RequestId',
     ];
 
@@ -32,9 +25,6 @@ class FeedbackSessionResponseBody extends Model
     public function toMap()
     {
         $res = [];
-        if (null !== $this->data) {
-            $res['Data'] = null !== $this->data ? $this->data->toMap() : null;
-        }
         if (null !== $this->requestId) {
             $res['RequestId'] = $this->requestId;
         }
@@ -50,9 +40,6 @@ class FeedbackSessionResponseBody extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
-        if (isset($map['Data'])) {
-            $model->data = data::fromMap($map['Data']);
-        }
         if (isset($map['RequestId'])) {
             $model->requestId = $map['RequestId'];
         }
