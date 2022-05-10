3644729da4b3694c2ec1fb12a159336e1312602c
sdk-team <sdk-team@alibabacloud.com>
2022-05-10 06:46:42 +0000

    Fix some bugs.

--- imp-20210630/src/Models/PublishLiveResponse.php
+++ imp-20210630/src/Models/PublishLiveResponse.php
@@ -14,17 +14,24 @@ class PublishLiveResponse extends Model
     public $headers;
 
     /**
+     * @var int
+     */
+    public $statusCode;
+
+    /**
      * @var PublishLiveResponseBody
      */
     public $body;
     protected $_name = [
-        'headers' => 'headers',
-        'body'    => 'body',
+        'headers'    => 'headers',
+        'statusCode' => 'statusCode',
+        'body'       => 'body',
     ];
 
     public function validate()
     {
         Model::validateRequired('headers', $this->headers, true);
+        Model::validateRequired('statusCode', $this->statusCode, true);
         Model::validateRequired('body', $this->body, true);
     }
 
@@ -33,6 +40,9 @@ class PublishLiveResponse extends Model
         $res = [];
         if (null !== $this->headers) {
             $res['headers'] = $this->headers;
+        }
+        if (null !== $this->statusCode) {
+            $res['statusCode'] = $this->statusCode;
         }
         if (null !== $this->body) {
             $res['body'] = null !== $this->body ? $this->body->toMap() : null;
@@ -51,6 +61,9 @@ class PublishLiveResponse extends Model
         $model = new self();
         if (isset($map['headers'])) {
             $model->headers = $map['headers'];
+        }
+        if (isset($map['statusCode'])) {
+            $model->statusCode = $map['statusCode'];
         }
         if (isset($map['body'])) {
             $model->body = PublishLiveResponseBody::fromMap($map['body']);
