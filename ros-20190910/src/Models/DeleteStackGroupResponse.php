f1ac10410c691a1440aa99fa3e63fbc0dd42c0ae
sdk-team <sdk-team@alibabacloud.com>
2022-05-19 07:12:14 +0000

    GetFeatureDetails supports ResourceCleaner.

--- ros-20190910/src/Models/DeleteStackGroupResponse.php
+++ ros-20190910/src/Models/DeleteStackGroupResponse.php
@@ -14,17 +14,24 @@ class DeleteStackGroupResponse extends Model
     public $headers;
 
     /**
+     * @var int
+     */
+    public $statusCode;
+
+    /**
      * @var DeleteStackGroupResponseBody
      */
     public $body;
     protected $_name = [
-        'headers' => 'headers',
-        'body'    => 'body',
+        'headers'    => 'headers',
+        'statusCode' => 'statusCode',
+        'body'       => 'body',
     ];
 
     public function validate()
     {
         Model::validateRequired('headers', $this->headers, true);
+        Model::validateRequired('statusCode', $this->statusCode, true);
         Model::validateRequired('body', $this->body, true);
     }
 
@@ -33,6 +40,9 @@ class DeleteStackGroupResponse extends Model
         $res = [];
         if (null !== $this->headers) {
             $res['headers'] = $this->headers;
+        }
+        if (null !== $this->statusCode) {
+            $res['statusCode'] = $this->statusCode;
         }
         if (null !== $this->body) {
             $res['body'] = null !== $this->body ? $this->body->toMap() : null;
@@ -51,6 +61,9 @@ class DeleteStackGroupResponse extends Model
         $model = new self();
         if (isset($map['headers'])) {
             $model->headers = $map['headers'];
+        }
+        if (isset($map['statusCode'])) {
+            $model->statusCode = $map['statusCode'];
         }
         if (isset($map['body'])) {
             $model->body = DeleteStackGroupResponseBody::fromMap($map['body']);
