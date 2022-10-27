a59fb2567f78d5b34a9fc16d58e8bffc2dc82baa
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:23:56 +0000

    Add DescribeWildcardDomainDetailData.

--- cdn-20180510/src/Models/SetCdnDomainStagingConfigResponseBody.php
+++ cdn-20180510/src/Models/SetCdnDomainStagingConfigResponseBody.php
@@ -4,16 +4,23 @@
 
 namespace AlibabaCloud\SDK\Cdn\V20180510\Models;
 
+use AlibabaCloud\SDK\Cdn\V20180510\Models\SetCdnDomainStagingConfigResponseBody\domainConfigList;
 use AlibabaCloud\Tea\Model;
 
 class SetCdnDomainStagingConfigResponseBody extends Model
 {
     /**
+     * @var domainConfigList[]
+     */
+    public $domainConfigList;
+
+    /**
      * @var string
      */
     public $requestId;
     protected $_name = [
-        'requestId' => 'RequestId',
+        'domainConfigList' => 'DomainConfigList',
+        'requestId'        => 'RequestId',
     ];
 
     public function validate()
@@ -23,6 +30,15 @@ class SetCdnDomainStagingConfigResponseBody extends Model
     public function toMap()
     {
         $res = [];
+        if (null !== $this->domainConfigList) {
+            $res['DomainConfigList'] = [];
+            if (null !== $this->domainConfigList && \is_array($this->domainConfigList)) {
+                $n = 0;
+                foreach ($this->domainConfigList as $item) {
+                    $res['DomainConfigList'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
+        }
         if (null !== $this->requestId) {
             $res['RequestId'] = $this->requestId;
         }
@@ -38,6 +54,15 @@ class SetCdnDomainStagingConfigResponseBody extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['DomainConfigList'])) {
+            if (!empty($map['DomainConfigList'])) {
+                $model->domainConfigList = [];
+                $n                       = 0;
+                foreach ($map['DomainConfigList'] as $item) {
+                    $model->domainConfigList[$n++] = null !== $item ? domainConfigList::fromMap($item) : $item;
+                }
+            }
+        }
         if (isset($map['RequestId'])) {
             $model->requestId = $map['RequestId'];
         }
---
+++ cdn-20180510/src/Models/SetCdnDomainStagingConfigResponseBody/domainConfigList.php
@@ -0,0 +1,71 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Cdn\V20180510\Models\SetCdnDomainStagingConfigResponseBody;
+
+use AlibabaCloud\Tea\Model;
+
+class domainConfigList extends Model
+{
+    /**
+     * @var int
+     */
+    public $configId;
+
+    /**
+     * @var string
+     */
+    public $domainName;
+
+    /**
+     * @var string
+     */
+    public $functionName;
+    protected $_name = [
+        'configId'     => 'ConfigId',
+        'domainName'   => 'DomainName',
+        'functionName' => 'FunctionName',
+    ];
+
+    public function validate()
+    {
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->configId) {
+            $res['ConfigId'] = $this->configId;
+        }
+        if (null !== $this->domainName) {
+            $res['DomainName'] = $this->domainName;
+        }
+        if (null !== $this->functionName) {
+            $res['FunctionName'] = $this->functionName;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return domainConfigList
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['ConfigId'])) {
+            $model->configId = $map['ConfigId'];
+        }
+        if (isset($map['DomainName'])) {
+            $model->domainName = $map['DomainName'];
+        }
+        if (isset($map['FunctionName'])) {
+            $model->functionName = $map['FunctionName'];
+        }
+
+        return $model;
+    }
+}
