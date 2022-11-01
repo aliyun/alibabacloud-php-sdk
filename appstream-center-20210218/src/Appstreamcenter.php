52319fcd30fb970d79f0b8833d10708f13cab0e8
sdk-team <sdk-team@alibabacloud.com>
2022-11-01 06:49:47 +0000

    AuthCode.

---
+++ appstream-center-20210218/src/Appstreamcenter.php
@@ -0,0 +1,43 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Appstreamcenter\V20210218;
+
+use AlibabaCloud\Endpoint\Endpoint;
+use AlibabaCloud\Tea\Utils\Utils;
+use Darabonba\OpenApi\OpenApiClient;
+
+class Appstreamcenter extends OpenApiClient
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+        $this->_endpointRule = '';
+        $this->checkConfig($config);
+        $this->_endpoint = $this->getEndpoint('appstream-center', $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
+    }
+
+    /**
+     * @param string   $productId
+     * @param string   $regionId
+     * @param string   $endpointRule
+     * @param string   $network
+     * @param string   $suffix
+     * @param string[] $endpointMap
+     * @param string   $endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint($productId, $regionId, $endpointRule, $network, $suffix, $endpointMap, $endpoint)
+    {
+        if (!Utils::empty_($endpoint)) {
+            return $endpoint;
+        }
+        if (!Utils::isUnset($endpointMap) && !Utils::empty_(@$endpointMap[$regionId])) {
+            return @$endpointMap[$regionId];
+        }
+
+        return Endpoint::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
+    }
+}
