aaf1616863107f92f9f40318d2f9f8f10a72345e
sdk-team <sdk-team@alibabacloud.com>
2022-12-07 07:22:35 +0000

    estimated price filed mod

--- btripopen-20220520/src/Models/EstimatedPriceQueryResponseBody/module/trafficFee/btripRoutes.php
+++ btripopen-20220520/src/Models/EstimatedPriceQueryResponseBody/module/trafficFee/btripRoutes.php
@@ -11,14 +11,14 @@ use AlibabaCloud\Tea\Model;
 class btripRoutes extends Model
 {
     /**
-     * @var int
+     * @var string
      */
-    public $arrDate;
+    public $arrCity;
 
     /**
      * @var int
      */
-    public $btripType;
+    public $arrDate;
 
     /**
      * @var cheapest
@@ -26,14 +26,14 @@ class btripRoutes extends Model
     public $cheapest;
 
     /**
+     * @var string
+     */
+    public $depCity;
+
+    /**
      * @var int
      */
     public $depDate;
-
-    /**
-     * @var string
-     */
-    public $destCity;
 
     /**
      * @var string
@@ -46,36 +46,24 @@ class btripRoutes extends Model
     public $itineraryId;
 
     /**
-     * @var int
-     */
-    public $itineraryIndex;
-
-    /**
      * @var mostExpensive
      */
     public $mostExpensive;
-
-    /**
-     * @var string
-     */
-    public $orgCity;
 
     /**
      * @var bool
      */
     public $success;
     protected $_name = [
-        'arrDate'        => 'arr_date',
-        'btripType'      => 'btrip_type',
-        'cheapest'       => 'cheapest',
-        'depDate'        => 'dep_date',
-        'destCity'       => 'dest_city',
-        'errMsg'         => 'err_msg',
-        'itineraryId'    => 'itinerary_id',
-        'itineraryIndex' => 'itinerary_index',
-        'mostExpensive'  => 'most_expensive',
-        'orgCity'        => 'org_city',
-        'success'        => 'success',
+        'arrCity'       => 'arr_city',
+        'arrDate'       => 'arr_date',
+        'cheapest'      => 'cheapest',
+        'depCity'       => 'dep_city',
+        'depDate'       => 'dep_date',
+        'errMsg'        => 'err_msg',
+        'itineraryId'   => 'itinerary_id',
+        'mostExpensive' => 'most_expensive',
+        'success'       => 'success',
     ];
 
     public function validate()
@@ -85,20 +73,20 @@ class btripRoutes extends Model
     public function toMap()
     {
         $res = [];
+        if (null !== $this->arrCity) {
+            $res['arr_city'] = $this->arrCity;
+        }
         if (null !== $this->arrDate) {
             $res['arr_date'] = $this->arrDate;
-        }
-        if (null !== $this->btripType) {
-            $res['btrip_type'] = $this->btripType;
         }
         if (null !== $this->cheapest) {
             $res['cheapest'] = null !== $this->cheapest ? $this->cheapest->toMap() : null;
         }
+        if (null !== $this->depCity) {
+            $res['dep_city'] = $this->depCity;
+        }
         if (null !== $this->depDate) {
             $res['dep_date'] = $this->depDate;
-        }
-        if (null !== $this->destCity) {
-            $res['dest_city'] = $this->destCity;
         }
         if (null !== $this->errMsg) {
             $res['err_msg'] = $this->errMsg;
@@ -106,14 +94,8 @@ class btripRoutes extends Model
         if (null !== $this->itineraryId) {
             $res['itinerary_id'] = $this->itineraryId;
         }
-        if (null !== $this->itineraryIndex) {
-            $res['itinerary_index'] = $this->itineraryIndex;
-        }
         if (null !== $this->mostExpensive) {
             $res['most_expensive'] = null !== $this->mostExpensive ? $this->mostExpensive->toMap() : null;
-        }
-        if (null !== $this->orgCity) {
-            $res['org_city'] = $this->orgCity;
         }
         if (null !== $this->success) {
             $res['success'] = $this->success;
@@ -130,20 +112,20 @@ class btripRoutes extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['arr_city'])) {
+            $model->arrCity = $map['arr_city'];
+        }
         if (isset($map['arr_date'])) {
             $model->arrDate = $map['arr_date'];
-        }
-        if (isset($map['btrip_type'])) {
-            $model->btripType = $map['btrip_type'];
         }
         if (isset($map['cheapest'])) {
             $model->cheapest = cheapest::fromMap($map['cheapest']);
         }
+        if (isset($map['dep_city'])) {
+            $model->depCity = $map['dep_city'];
+        }
         if (isset($map['dep_date'])) {
             $model->depDate = $map['dep_date'];
-        }
-        if (isset($map['dest_city'])) {
-            $model->destCity = $map['dest_city'];
         }
         if (isset($map['err_msg'])) {
             $model->errMsg = $map['err_msg'];
@@ -151,14 +133,8 @@ class btripRoutes extends Model
         if (isset($map['itinerary_id'])) {
             $model->itineraryId = $map['itinerary_id'];
         }
-        if (isset($map['itinerary_index'])) {
-            $model->itineraryIndex = $map['itinerary_index'];
-        }
         if (isset($map['most_expensive'])) {
             $model->mostExpensive = mostExpensive::fromMap($map['most_expensive']);
-        }
-        if (isset($map['org_city'])) {
-            $model->orgCity = $map['org_city'];
         }
         if (isset($map['success'])) {
             $model->success = $map['success'];
