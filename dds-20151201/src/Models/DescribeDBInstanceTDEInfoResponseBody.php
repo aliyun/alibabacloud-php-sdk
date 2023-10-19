bcfd4fa722508c31b61854e3f956000d549de085
sdk-team <sdk-team@alibabacloud.com>
2023-10-19 17:14:21 +0000

    Generated php 2015-12-01 for Dds.

--- dds-20151201/src/Models/DescribeDBInstanceTDEInfoResponseBody.php
+++ dds-20151201/src/Models/DescribeDBInstanceTDEInfoResponseBody.php
@@ -9,11 +9,26 @@ use AlibabaCloud\Tea\Model;
 class DescribeDBInstanceTDEInfoResponseBody extends Model
 {
     /**
+     * @var string
+     */
+    public $encryptionKey;
+
+    /**
+     * @var string
+     */
+    public $encryptorName;
+
+    /**
      * @example F4DD0E29-361B-42F2-9301-B0048CCCE5D6
      *
      * @var string
      */
     public $requestId;
+
+    /**
+     * @var string
+     */
+    public $roleARN;
 
     /**
      * @example enabled
@@ -22,8 +37,11 @@ class DescribeDBInstanceTDEInfoResponseBody extends Model
      */
     public $TDEStatus;
     protected $_name = [
-        'requestId' => 'RequestId',
-        'TDEStatus' => 'TDEStatus',
+        'encryptionKey' => 'EncryptionKey',
+        'encryptorName' => 'EncryptorName',
+        'requestId'     => 'RequestId',
+        'roleARN'       => 'RoleARN',
+        'TDEStatus'     => 'TDEStatus',
     ];
 
     public function validate()
@@ -33,8 +51,17 @@ class DescribeDBInstanceTDEInfoResponseBody extends Model
     public function toMap()
     {
         $res = [];
+        if (null !== $this->encryptionKey) {
+            $res['EncryptionKey'] = $this->encryptionKey;
+        }
+        if (null !== $this->encryptorName) {
+            $res['EncryptorName'] = $this->encryptorName;
+        }
         if (null !== $this->requestId) {
             $res['RequestId'] = $this->requestId;
+        }
+        if (null !== $this->roleARN) {
+            $res['RoleARN'] = $this->roleARN;
         }
         if (null !== $this->TDEStatus) {
             $res['TDEStatus'] = $this->TDEStatus;
@@ -51,8 +78,17 @@ class DescribeDBInstanceTDEInfoResponseBody extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['EncryptionKey'])) {
+            $model->encryptionKey = $map['EncryptionKey'];
+        }
+        if (isset($map['EncryptorName'])) {
+            $model->encryptorName = $map['EncryptorName'];
+        }
         if (isset($map['RequestId'])) {
             $model->requestId = $map['RequestId'];
+        }
+        if (isset($map['RoleARN'])) {
+            $model->roleARN = $map['RoleARN'];
         }
         if (isset($map['TDEStatus'])) {
             $model->TDEStatus = $map['TDEStatus'];
