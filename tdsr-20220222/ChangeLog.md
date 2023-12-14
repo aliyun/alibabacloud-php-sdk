519cac17e81b2bcc6f9f68f76f14fa613d3cde73
sdk-team <sdk-team@alibabacloud.com>
2023-12-14 12:13:44 +0000

    Generated php 2022-02-22 for tdsr.

---
+++ tdsr-20220222/ChangeLog.md
@@ -0,0 +1,3 @@
+2023-12-14 Version: 1.0.0
+- Generated php 2022-02-22 for tdsr.
+
---
+++ tdsr-20220222/src/Tdsr.php
@@ -0,0 +1,46 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Tdsr\V20220222;
+
+use AlibabaCloud\Endpoint\Endpoint;
+use AlibabaCloud\Tea\Utils\Utils;
+use Darabonba\OpenApi\OpenApiClient;
+
+class Tdsr extends OpenApiClient
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+        $this->_endpointRule = 'regional';
+        $this->_endpointMap  = [
+            'cn-hangzhou' => 'lyj.cn-hangzhou.aliyuncs.com',
+        ];
+        $this->checkConfig($config);
+        $this->_endpoint = $this->getEndpoint('tdsr', $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
+    }
+
+    /**
+     * @param string   $productId
+     * @param string   $regionId
+     * @param string   $endpointRule
+     * @param string   $network
+     * @param string   $suffix
+     * @param string[] $endpointMap
+     * @param string   $endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint($productId, $regionId, $endpointRule, $network, $suffix, $endpointMap, $endpoint)
+    {
+        if (!Utils::empty_($endpoint)) {
+            return $endpoint;
+        }
+        if (!Utils::isUnset($endpointMap) && !Utils::empty_(@$endpointMap[$regionId])) {
+            return @$endpointMap[$regionId];
+        }
+
+        return Endpoint::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
+    }
+}
