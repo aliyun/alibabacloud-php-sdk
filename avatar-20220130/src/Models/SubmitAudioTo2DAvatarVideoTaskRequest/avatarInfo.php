6d37dc14e01378934ed4ae882f3564cf7c606b9b
sdk-team <sdk-team@alibabacloud.com>
2023-12-14 06:30:35 +0000

    Generated php 2022-01-30 for avatar.

--- avatar-20220130/src/Models/SubmitAudioTo2DAvatarVideoTaskRequest/avatarInfo.php
+++ avatar-20220130/src/Models/SubmitAudioTo2DAvatarVideoTaskRequest/avatarInfo.php
@@ -14,8 +14,32 @@ class avatarInfo extends Model
      * @var string
      */
     public $code;
+
+    /**
+     * @var int
+     */
+    public $height;
+
+    /**
+     * @var int
+     */
+    public $width;
+
+    /**
+     * @var int
+     */
+    public $x;
+
+    /**
+     * @var int
+     */
+    public $y;
     protected $_name = [
-        'code' => 'Code',
+        'code'   => 'Code',
+        'height' => 'Height',
+        'width'  => 'Width',
+        'x'      => 'X',
+        'y'      => 'Y',
     ];
 
     public function validate()
@@ -27,6 +51,18 @@ class avatarInfo extends Model
         $res = [];
         if (null !== $this->code) {
             $res['Code'] = $this->code;
+        }
+        if (null !== $this->height) {
+            $res['Height'] = $this->height;
+        }
+        if (null !== $this->width) {
+            $res['Width'] = $this->width;
+        }
+        if (null !== $this->x) {
+            $res['X'] = $this->x;
+        }
+        if (null !== $this->y) {
+            $res['Y'] = $this->y;
         }
 
         return $res;
@@ -42,6 +78,18 @@ class avatarInfo extends Model
         $model = new self();
         if (isset($map['Code'])) {
             $model->code = $map['Code'];
+        }
+        if (isset($map['Height'])) {
+            $model->height = $map['Height'];
+        }
+        if (isset($map['Width'])) {
+            $model->width = $map['Width'];
+        }
+        if (isset($map['X'])) {
+            $model->x = $map['X'];
+        }
+        if (isset($map['Y'])) {
+            $model->y = $map['Y'];
         }
 
         return $model;
