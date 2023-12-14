63894d888218488c05e2c8cf891e7d91070f2699
sdk-team <sdk-team@alibabacloud.com>
2023-12-14 12:15:22 +0000

    Generated php 2021-11-22 for ververica.

---
+++ ververica-20211122/ChangeLog.md
@@ -0,0 +1,3 @@
+2023-12-14 Version: 1.0.0
+- Generated php 2021-11-22 for ververica.
+
---
+++ ververica-20211122/src/Ververica.php
@@ -0,0 +1,43 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Ververica\V20211122;
+
+use AlibabaCloud\Endpoint\Endpoint;
+use AlibabaCloud\Tea\Utils\Utils;
+use Darabonba\OpenApi\OpenApiClient;
+
+class Ververica extends OpenApiClient
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+        $this->_endpointRule = 'regional';
+        $this->checkConfig($config);
+        $this->_endpoint = $this->getEndpoint('ververica', $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
+    }
+
+    /**
+     * @param string   $productId
+     * @param string   $regionId
+     * @param string   $endpointRule
+     * @param string   $network
+     * @param string   $suffix
+     * @param string[] $endpointMap
+     * @param string   $endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint($productId, $regionId, $endpointRule, $network, $suffix, $endpointMap, $endpoint)
+    {
+        if (!Utils::empty_($endpoint)) {
+            return $endpoint;
+        }
+        if (!Utils::isUnset($endpointMap) && !Utils::empty_(@$endpointMap[$regionId])) {
+            return @$endpointMap[$regionId];
+        }
+
+        return Endpoint::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
+    }
+}
