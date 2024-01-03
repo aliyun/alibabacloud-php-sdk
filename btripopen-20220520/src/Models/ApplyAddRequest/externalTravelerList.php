5860c087c94871f566e50b5216221b6376823031
sdk-team <sdk-team@alibabacloud.com>
2024-01-03 17:12:35 +0000

    Generated php 2022-05-20 for btripOpen.

--- btripopen-20220520/src/Models/ApplyAddRequest/externalTravelerList.php
+++ btripopen-20220520/src/Models/ApplyAddRequest/externalTravelerList.php
@@ -12,8 +12,14 @@ class externalTravelerList extends Model
      * @var string
      */
     public $userName;
+
+    /**
+     * @var string
+     */
+    public $userNameEn;
     protected $_name = [
-        'userName' => 'user_name',
+        'userName'   => 'user_name',
+        'userNameEn' => 'user_name_en',
     ];
 
     public function validate()
@@ -25,6 +31,9 @@ class externalTravelerList extends Model
         $res = [];
         if (null !== $this->userName) {
             $res['user_name'] = $this->userName;
+        }
+        if (null !== $this->userNameEn) {
+            $res['user_name_en'] = $this->userNameEn;
         }
 
         return $res;
@@ -40,6 +49,9 @@ class externalTravelerList extends Model
         $model = new self();
         if (isset($map['user_name'])) {
             $model->userName = $map['user_name'];
+        }
+        if (isset($map['user_name_en'])) {
+            $model->userNameEn = $map['user_name_en'];
         }
 
         return $model;
