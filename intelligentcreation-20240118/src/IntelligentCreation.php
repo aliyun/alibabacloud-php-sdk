cf7f14485e268e7ff28dff637946eb38d3a252f7
sdk-team <sdk-team@alibabacloud.com>
2024-01-04 12:16:50 +0000

    Generated php 2024-01-18 for IntelligentCreation.

--- intelligentcreation-20240118/src/IntelligentCreation.php
+++ intelligentcreation-20240118/src/IntelligentCreation.php
@@ -5,7 +5,17 @@
 namespace AlibabaCloud\SDK\IntelligentCreation\V20240118;
 
 use AlibabaCloud\Endpoint\Endpoint;
+use AlibabaCloud\OpenApiUtil\OpenApiUtilClient;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\ActualDeductResourceRequest;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\ActualDeductResourceResponse;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\DirectDeductResourceRequest;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\DirectDeductResourceResponse;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\ExpectDeductResourceRequest;
+use AlibabaCloud\SDK\IntelligentCreation\V20240118\Models\ExpectDeductResourceResponse;
 use AlibabaCloud\Tea\Utils\Utils;
+use AlibabaCloud\Tea\Utils\Utils\RuntimeOptions;
+use Darabonba\OpenApi\Models\OpenApiRequest;
+use Darabonba\OpenApi\Models\Params;
 use Darabonba\OpenApi\OpenApiClient;
 
 class IntelligentCreation extends OpenApiClient
@@ -39,5 +49,131 @@ class IntelligentCreation extends OpenApiClient
         }
 
         return Endpoint::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
+    }
+
+    /**
+     * @param ActualDeductResourceRequest $request
+     * @param string[]                    $headers
+     * @param RuntimeOptions              $runtime
+     *
+     * @return ActualDeductResourceResponse
+     */
+    public function actualDeductResourceWithOptions($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+        $req = new OpenApiRequest([
+            'headers' => $headers,
+            'body'    => OpenApiUtilClient::parseToMap($request->body),
+        ]);
+        $params = new Params([
+            'action'      => 'ActualDeductResource',
+            'version'     => '2024-01-18',
+            'protocol'    => 'HTTPS',
+            'pathname'    => '/yic/yic-console/v1/digital-human/commands/actual-deduct',
+            'method'      => 'POST',
+            'authType'    => 'AK',
+            'style'       => 'ROA',
+            'reqBodyType' => 'json',
+            'bodyType'    => 'json',
+        ]);
+
+        return ActualDeductResourceResponse::fromMap($this->callApi($params, $req, $runtime));
+    }
+
+    /**
+     * @param ActualDeductResourceRequest $request
+     *
+     * @return ActualDeductResourceResponse
+     */
+    public function actualDeductResource($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->actualDeductResourceWithOptions($request, $headers, $runtime);
+    }
+
+    /**
+     * @param DirectDeductResourceRequest $request
+     * @param string[]                    $headers
+     * @param RuntimeOptions              $runtime
+     *
+     * @return DirectDeductResourceResponse
+     */
+    public function directDeductResourceWithOptions($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+        $req = new OpenApiRequest([
+            'headers' => $headers,
+            'body'    => OpenApiUtilClient::parseToMap($request->body),
+        ]);
+        $params = new Params([
+            'action'      => 'DirectDeductResource',
+            'version'     => '2024-01-18',
+            'protocol'    => 'HTTPS',
+            'pathname'    => '/yic/yic-console/v1/digital-human/commands/direct-deduct',
+            'method'      => 'POST',
+            'authType'    => 'AK',
+            'style'       => 'ROA',
+            'reqBodyType' => 'json',
+            'bodyType'    => 'json',
+        ]);
+
+        return DirectDeductResourceResponse::fromMap($this->callApi($params, $req, $runtime));
+    }
+
+    /**
+     * @param DirectDeductResourceRequest $request
+     *
+     * @return DirectDeductResourceResponse
+     */
+    public function directDeductResource($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->directDeductResourceWithOptions($request, $headers, $runtime);
+    }
+
+    /**
+     * @param ExpectDeductResourceRequest $request
+     * @param string[]                    $headers
+     * @param RuntimeOptions              $runtime
+     *
+     * @return ExpectDeductResourceResponse
+     */
+    public function expectDeductResourceWithOptions($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+        $req = new OpenApiRequest([
+            'headers' => $headers,
+            'body'    => OpenApiUtilClient::parseToMap($request->body),
+        ]);
+        $params = new Params([
+            'action'      => 'ExpectDeductResource',
+            'version'     => '2024-01-18',
+            'protocol'    => 'HTTPS',
+            'pathname'    => '/yic/yic-console/v1/digital-human/commands/expect-deduct',
+            'method'      => 'POST',
+            'authType'    => 'AK',
+            'style'       => 'ROA',
+            'reqBodyType' => 'json',
+            'bodyType'    => 'json',
+        ]);
+
+        return ExpectDeductResourceResponse::fromMap($this->callApi($params, $req, $runtime));
+    }
+
+    /**
+     * @param ExpectDeductResourceRequest $request
+     *
+     * @return ExpectDeductResourceResponse
+     */
+    public function expectDeductResource($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->expectDeductResourceWithOptions($request, $headers, $runtime);
     }
 }
