6633bb1e11e648b7bfb8d55f3464c0a41593280c
sdk-team <sdk-team@alibabacloud.com>
2024-03-11 17:13:30 +0000

    Update API ADMiniCog: update response param.

--- alinlp-20200629/src/Models/GetPosChGeneralResponse.php
+++ alinlp-20200629/src/Models/GetPosChGeneralResponse.php
@@ -30,9 +30,6 @@ class GetPosChGeneralResponse extends Model
 
     public function validate()
     {
-        Model::validateRequired('headers', $this->headers, true);
-        Model::validateRequired('statusCode', $this->statusCode, true);
-        Model::validateRequired('body', $this->body, true);
     }
 
     public function toMap()
