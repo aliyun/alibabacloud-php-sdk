4374ed232104102bf786fd6640bdfff866038c0a
sdk-team <sdk-team@alibabacloud.com>
2024-05-27 08:15:10 +0000

    Update API TextModerationPlus: update response param.

--- green-20220302/src/Models/TextModerationPlusResponseBody/data/advice.php
+++ green-20220302/src/Models/TextModerationPlusResponseBody/data/advice.php
@@ -14,8 +14,20 @@ class advice extends Model
      * @var string
      */
     public $answer;
+
+    /**
+     * @var string
+     */
+    public $hitLabel;
+
+    /**
+     * @var string
+     */
+    public $hitLibName;
     protected $_name = [
-        'answer' => 'Answer',
+        'answer'     => 'Answer',
+        'hitLabel'   => 'HitLabel',
+        'hitLibName' => 'HitLibName',
     ];
 
     public function validate()
@@ -27,6 +39,12 @@ class advice extends Model
         $res = [];
         if (null !== $this->answer) {
             $res['Answer'] = $this->answer;
+        }
+        if (null !== $this->hitLabel) {
+            $res['HitLabel'] = $this->hitLabel;
+        }
+        if (null !== $this->hitLibName) {
+            $res['HitLibName'] = $this->hitLibName;
         }
 
         return $res;
@@ -42,6 +60,12 @@ class advice extends Model
         $model = new self();
         if (isset($map['Answer'])) {
             $model->answer = $map['Answer'];
+        }
+        if (isset($map['HitLabel'])) {
+            $model->hitLabel = $map['HitLabel'];
+        }
+        if (isset($map['HitLibName'])) {
+            $model->hitLibName = $map['HitLibName'];
         }
 
         return $model;
