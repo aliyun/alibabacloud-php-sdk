48962d85f9f3f6799ccd3df7aa90cd7e47e40574
sdk-team <sdk-team@alibabacloud.com>
2024-07-23 17:14:01 +0000

    Update API DescribeCloudDriveGroups: update response param.

--- ecd-20200930/src/Models/DescribeCloudDriveGroupsResponseBody/cloudDriveGroups/adminUserInfos.php
+++ ecd-20200930/src/Models/DescribeCloudDriveGroupsResponseBody/cloudDriveGroups/adminUserInfos.php
@@ -11,15 +11,45 @@ class adminUserInfos extends Model
     /**
      * @var string
      */
+    public $email;
+
+    /**
+     * @var string
+     */
     public $endUserId;
 
     /**
      * @var string
      */
+    public $jobNumber;
+
+    /**
+     * @var string
+     */
     public $nickName;
+
+    /**
+     * @var string
+     */
+    public $phone;
+
+    /**
+     * @var string
+     */
+    public $realNickName;
+
+    /**
+     * @var string
+     */
+    public $remark;
     protected $_name = [
-        'endUserId' => 'EndUserId',
-        'nickName'  => 'NickName',
+        'email'        => 'Email',
+        'endUserId'    => 'EndUserId',
+        'jobNumber'    => 'JobNumber',
+        'nickName'     => 'NickName',
+        'phone'        => 'Phone',
+        'realNickName' => 'RealNickName',
+        'remark'       => 'Remark',
     ];
 
     public function validate()
@@ -29,11 +59,26 @@ class adminUserInfos extends Model
     public function toMap()
     {
         $res = [];
+        if (null !== $this->email) {
+            $res['Email'] = $this->email;
+        }
         if (null !== $this->endUserId) {
             $res['EndUserId'] = $this->endUserId;
         }
+        if (null !== $this->jobNumber) {
+            $res['JobNumber'] = $this->jobNumber;
+        }
         if (null !== $this->nickName) {
             $res['NickName'] = $this->nickName;
+        }
+        if (null !== $this->phone) {
+            $res['Phone'] = $this->phone;
+        }
+        if (null !== $this->realNickName) {
+            $res['RealNickName'] = $this->realNickName;
+        }
+        if (null !== $this->remark) {
+            $res['Remark'] = $this->remark;
         }
 
         return $res;
@@ -47,11 +92,26 @@ class adminUserInfos extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['Email'])) {
+            $model->email = $map['Email'];
+        }
         if (isset($map['EndUserId'])) {
             $model->endUserId = $map['EndUserId'];
         }
+        if (isset($map['JobNumber'])) {
+            $model->jobNumber = $map['JobNumber'];
+        }
         if (isset($map['NickName'])) {
             $model->nickName = $map['NickName'];
+        }
+        if (isset($map['Phone'])) {
+            $model->phone = $map['Phone'];
+        }
+        if (isset($map['RealNickName'])) {
+            $model->realNickName = $map['RealNickName'];
+        }
+        if (isset($map['Remark'])) {
+            $model->remark = $map['Remark'];
         }
 
         return $model;
