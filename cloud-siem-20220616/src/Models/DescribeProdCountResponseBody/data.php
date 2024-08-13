ab7decdbf0c8a5a12c92b81d3c02c955054fda94
sdk-team <sdk-team@alibabacloud.com>
2024-08-13 17:13:04 +0000

    Update API DescribeProdCount: update param RoleType.

--- cloud-siem-20220616/src/Models/DescribeProdCountResponseBody/data.php
+++ cloud-siem-20220616/src/Models/DescribeProdCountResponseBody/data.php
@@ -9,6 +9,11 @@ use AlibabaCloud\Tea\Model;
 class data extends Model
 {
     /**
+     * @var int
+     */
+    public $aliyunImportedCount;
+
+    /**
      * @description The number of Alibaba Cloud services.
      *
      * @example 19
@@ -16,6 +21,11 @@ class data extends Model
      * @var int
      */
     public $aliyunProdCount;
+
+    /**
+     * @var int
+     */
+    public $hcloudImportedCount;
 
     /**
      * @description The number of Huawei Cloud services.
@@ -27,6 +37,23 @@ class data extends Model
     public $hcloudProdCount;
 
     /**
+     * @var int
+     */
+    public $idcImportedCount;
+
+    /**
+     * @example 2
+     *
+     * @var int
+     */
+    public $idcProdCount;
+
+    /**
+     * @var int
+     */
+    public $qcloudImportedCount;
+
+    /**
      * @description The number of Tencent Cloud services.
      *
      * @example 2
@@ -35,9 +62,14 @@ class data extends Model
      */
     public $qcloudProdCount;
     protected $_name = [
-        'aliyunProdCount' => 'AliyunProdCount',
-        'hcloudProdCount' => 'HcloudProdCount',
-        'qcloudProdCount' => 'QcloudProdCount',
+        'aliyunImportedCount' => 'AliyunImportedCount',
+        'aliyunProdCount'     => 'AliyunProdCount',
+        'hcloudImportedCount' => 'HcloudImportedCount',
+        'hcloudProdCount'     => 'HcloudProdCount',
+        'idcImportedCount'    => 'IdcImportedCount',
+        'idcProdCount'        => 'IdcProdCount',
+        'qcloudImportedCount' => 'QcloudImportedCount',
+        'qcloudProdCount'     => 'QcloudProdCount',
     ];
 
     public function validate()
@@ -47,11 +79,26 @@ class data extends Model
     public function toMap()
     {
         $res = [];
+        if (null !== $this->aliyunImportedCount) {
+            $res['AliyunImportedCount'] = $this->aliyunImportedCount;
+        }
         if (null !== $this->aliyunProdCount) {
             $res['AliyunProdCount'] = $this->aliyunProdCount;
         }
+        if (null !== $this->hcloudImportedCount) {
+            $res['HcloudImportedCount'] = $this->hcloudImportedCount;
+        }
         if (null !== $this->hcloudProdCount) {
             $res['HcloudProdCount'] = $this->hcloudProdCount;
+        }
+        if (null !== $this->idcImportedCount) {
+            $res['IdcImportedCount'] = $this->idcImportedCount;
+        }
+        if (null !== $this->idcProdCount) {
+            $res['IdcProdCount'] = $this->idcProdCount;
+        }
+        if (null !== $this->qcloudImportedCount) {
+            $res['QcloudImportedCount'] = $this->qcloudImportedCount;
         }
         if (null !== $this->qcloudProdCount) {
             $res['QcloudProdCount'] = $this->qcloudProdCount;
@@ -68,11 +115,26 @@ class data extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['AliyunImportedCount'])) {
+            $model->aliyunImportedCount = $map['AliyunImportedCount'];
+        }
         if (isset($map['AliyunProdCount'])) {
             $model->aliyunProdCount = $map['AliyunProdCount'];
         }
+        if (isset($map['HcloudImportedCount'])) {
+            $model->hcloudImportedCount = $map['HcloudImportedCount'];
+        }
         if (isset($map['HcloudProdCount'])) {
             $model->hcloudProdCount = $map['HcloudProdCount'];
+        }
+        if (isset($map['IdcImportedCount'])) {
+            $model->idcImportedCount = $map['IdcImportedCount'];
+        }
+        if (isset($map['IdcProdCount'])) {
+            $model->idcProdCount = $map['IdcProdCount'];
+        }
+        if (isset($map['QcloudImportedCount'])) {
+            $model->qcloudImportedCount = $map['QcloudImportedCount'];
         }
         if (isset($map['QcloudProdCount'])) {
             $model->qcloudProdCount = $map['QcloudProdCount'];
