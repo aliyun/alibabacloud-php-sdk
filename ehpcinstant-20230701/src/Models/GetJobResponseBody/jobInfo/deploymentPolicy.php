08ce394160c3c49bd89e5b9069c7163a4d5c51d7
sdk-team <sdk-team@alibabacloud.com>
2024-11-13 17:23:48 +0000

    Update API GetJob: update response param.

--- ehpcinstant-20230701/src/Models/GetJobResponseBody/jobInfo/deploymentPolicy.php
+++ ehpcinstant-20230701/src/Models/GetJobResponseBody/jobInfo/deploymentPolicy.php
@@ -5,6 +5,7 @@
 namespace AlibabaCloud\SDK\EhpcInstant\V20230701\Models\GetJobResponseBody\jobInfo;
 
 use AlibabaCloud\SDK\EhpcInstant\V20230701\Models\GetJobResponseBody\jobInfo\deploymentPolicy\network;
+use AlibabaCloud\SDK\EhpcInstant\V20230701\Models\GetJobResponseBody\jobInfo\deploymentPolicy\tags;
 use AlibabaCloud\Tea\Model;
 
 class deploymentPolicy extends Model
@@ -20,9 +21,15 @@ class deploymentPolicy extends Model
      * @var network
      */
     public $network;
+
+    /**
+     * @var tags[]
+     */
+    public $tags;
     protected $_name = [
         'allocationSpec' => 'AllocationSpec',
         'network'        => 'Network',
+        'tags'           => 'Tags',
     ];
 
     public function validate()
@@ -37,6 +44,15 @@ class deploymentPolicy extends Model
         }
         if (null !== $this->network) {
             $res['Network'] = null !== $this->network ? $this->network->toMap() : null;
+        }
+        if (null !== $this->tags) {
+            $res['Tags'] = [];
+            if (null !== $this->tags && \is_array($this->tags)) {
+                $n = 0;
+                foreach ($this->tags as $item) {
+                    $res['Tags'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
         }
 
         return $res;
@@ -55,6 +71,15 @@ class deploymentPolicy extends Model
         }
         if (isset($map['Network'])) {
             $model->network = network::fromMap($map['Network']);
+        }
+        if (isset($map['Tags'])) {
+            if (!empty($map['Tags'])) {
+                $model->tags = [];
+                $n           = 0;
+                foreach ($map['Tags'] as $item) {
+                    $model->tags[$n++] = null !== $item ? tags::fromMap($item) : $item;
+                }
+            }
         }
 
         return $model;
