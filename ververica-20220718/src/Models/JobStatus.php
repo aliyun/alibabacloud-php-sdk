defcbd82c45b3bb6cf2a8802428f10360ed505bc
sdk-team <sdk-team@alibabacloud.com>
2024-12-05 02:49:47 +0000

    Generated php 2022-07-18 for ververica.

--- ververica-20220718/src/Models/JobStatus.php
+++ ververica-20220718/src/Models/JobStatus.php
@@ -21,12 +21,24 @@ class JobStatus extends Model
     public $failure;
 
     /**
+     * @var int
+     */
+    public $healthScore;
+
+    /**
+     * @var string
+     */
+    public $riskLevel;
+
+    /**
      * @var JobStatusRunning
      */
     public $running;
     protected $_name = [
         'currentJobStatus' => 'currentJobStatus',
         'failure'          => 'failure',
+        'healthScore'      => 'healthScore',
+        'riskLevel'        => 'riskLevel',
         'running'          => 'running',
     ];
 
@@ -42,6 +54,12 @@ class JobStatus extends Model
         }
         if (null !== $this->failure) {
             $res['failure'] = null !== $this->failure ? $this->failure->toMap() : null;
+        }
+        if (null !== $this->healthScore) {
+            $res['healthScore'] = $this->healthScore;
+        }
+        if (null !== $this->riskLevel) {
+            $res['riskLevel'] = $this->riskLevel;
         }
         if (null !== $this->running) {
             $res['running'] = null !== $this->running ? $this->running->toMap() : null;
@@ -63,6 +81,12 @@ class JobStatus extends Model
         }
         if (isset($map['failure'])) {
             $model->failure = JobFailure::fromMap($map['failure']);
+        }
+        if (isset($map['healthScore'])) {
+            $model->healthScore = $map['healthScore'];
+        }
+        if (isset($map['riskLevel'])) {
+            $model->riskLevel = $map['riskLevel'];
         }
         if (isset($map['running'])) {
             $model->running = JobStatusRunning::fromMap($map['running']);
