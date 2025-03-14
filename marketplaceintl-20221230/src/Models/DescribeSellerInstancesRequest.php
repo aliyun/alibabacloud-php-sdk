7d3346f4038477a18ff8b6bcd0c7d890adb27622
sdk-team <sdk-team@alibabacloud.com>
2025-03-14 08:24:39 +0000

    Update API DescribeSellerInstances: update param PageIndex.

--- marketplaceintl-20221230/src/Models/DescribeSellerInstancesRequest.php
+++ marketplaceintl-20221230/src/Models/DescribeSellerInstancesRequest.php
@@ -1,16 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+// This file is auto-generated, don't edit it. Thanks.
 
 namespace AlibabaCloud\SDK\MarketplaceIntl\V20221230\Models;
 
@@ -60,7 +50,7 @@ class DescribeSellerInstancesRequest extends Model
         'userId' => 'UserId',
     ];
 
-    public function validate(): void {}
+    public function validate() {}
 
     public function toMap()
     {
