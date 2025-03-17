707d2a4ff3f25b09f11830fe37172ad326965621
sdk-team <sdk-team@alibabacloud.com>
2025-03-17 06:40:16 +0000

    Update API RunHotTopicSummary: update param stepForCustomSummaryStyleConfig.

--- quanmiaolightapp-20240801/src/Models/RunHotTopicSummaryRequest/stepForCustomSummaryStyleConfig.php
+++ quanmiaolightapp-20240801/src/Models/RunHotTopicSummaryRequest/stepForCustomSummaryStyleConfig.php
@@ -1,16 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
+// This file is auto-generated, don't edit it. Thanks.
 
 namespace AlibabaCloud\SDK\QuanMiaoLightApp\V20240801\Models\RunHotTopicSummaryRequest;
 
@@ -26,8 +16,6 @@ class stepForCustomSummaryStyleConfig extends Model
     public $summaryImageCount;
 
     /**
-     * @description This parameter is required.
-     *
      * @example qwen-max
      *
      * @var string
@@ -46,7 +34,7 @@ class stepForCustomSummaryStyleConfig extends Model
         'summaryPrompt' => 'summaryPrompt',
     ];
 
-    public function validate(): void {}
+    public function validate() {}
 
     public function toMap()
     {
