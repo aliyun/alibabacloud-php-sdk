6f71a93b257add0ea971f1ce14b9080854fffa90
sdk-team <sdk-team@alibabacloud.com>
2025-04-02 08:48:01 +0000

    Update API GetDialogAnalysisResult: add response parameters Body.data.dialogAnalysisRespList.$.analysisResp.dialogOpenAnalysis.

--- dianjin-20240628/src/Models/GetDialogAnalysisResultResponseBody/data/dialogAnalysisRespList/analysisResp.php
+++ dianjin-20240628/src/Models/GetDialogAnalysisResultResponseBody/data/dialogAnalysisRespList/analysisResp.php
@@ -20,6 +20,16 @@ class analysisResp extends Model
     public $dialogLabels;
 
     /**
+     * @var mixed[]
+     */
+    public $dialogOpenAnalysis;
+
+    /**
+     * @var mixed[]
+     */
+    public $dialogProcessAnalysis;
+
+    /**
      * @var string
      */
     public $dialogSop;
@@ -31,6 +41,8 @@ class analysisResp extends Model
     protected $_name = [
         'dialogExecPlan' => 'dialogExecPlan',
         'dialogLabels' => 'dialogLabels',
+        'dialogOpenAnalysis' => 'dialogOpenAnalysis',
+        'dialogProcessAnalysis' => 'dialogProcessAnalysis',
         'dialogSop' => 'dialogSop',
         'dialogSummary' => 'dialogSummary',
     ];
@@ -39,6 +51,12 @@ class analysisResp extends Model
     {
         if (\is_array($this->dialogLabels)) {
             Model::validateArray($this->dialogLabels);
+        }
+        if (\is_array($this->dialogOpenAnalysis)) {
+            Model::validateArray($this->dialogOpenAnalysis);
+        }
+        if (\is_array($this->dialogProcessAnalysis)) {
+            Model::validateArray($this->dialogProcessAnalysis);
         }
         parent::validate();
     }
@@ -56,6 +74,24 @@ class analysisResp extends Model
                 $n1 = 0;
                 foreach ($this->dialogLabels as $item1) {
                     $res['dialogLabels'][$n1++] = null !== $item1 ? $item1->toArray($noStream) : $item1;
+                }
+            }
+        }
+
+        if (null !== $this->dialogOpenAnalysis) {
+            if (\is_array($this->dialogOpenAnalysis)) {
+                $res['dialogOpenAnalysis'] = [];
+                foreach ($this->dialogOpenAnalysis as $key1 => $value1) {
+                    $res['dialogOpenAnalysis'][$key1] = $value1;
+                }
+            }
+        }
+
+        if (null !== $this->dialogProcessAnalysis) {
+            if (\is_array($this->dialogProcessAnalysis)) {
+                $res['dialogProcessAnalysis'] = [];
+                foreach ($this->dialogProcessAnalysis as $key1 => $value1) {
+                    $res['dialogProcessAnalysis'][$key1] = $value1;
                 }
             }
         }
@@ -89,6 +125,24 @@ class analysisResp extends Model
                 $n1 = 0;
                 foreach ($map['dialogLabels'] as $item1) {
                     $model->dialogLabels[$n1++] = dialogLabels::fromMap($item1);
+                }
+            }
+        }
+
+        if (isset($map['dialogOpenAnalysis'])) {
+            if (!empty($map['dialogOpenAnalysis'])) {
+                $model->dialogOpenAnalysis = [];
+                foreach ($map['dialogOpenAnalysis'] as $key1 => $value1) {
+                    $model->dialogOpenAnalysis[$key1] = $value1;
+                }
+            }
+        }
+
+        if (isset($map['dialogProcessAnalysis'])) {
+            if (!empty($map['dialogProcessAnalysis'])) {
+                $model->dialogProcessAnalysis = [];
+                foreach ($map['dialogProcessAnalysis'] as $key1 => $value1) {
+                    $model->dialogProcessAnalysis[$key1] = $value1;
                 }
             }
         }
