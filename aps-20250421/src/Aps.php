4953888835aec6c9b96753111b75ef54145c1189
sdk-team <sdk-team@alibabacloud.com>
2025-04-22 03:15:30 +0000

    Generated php 2025-04-21 for aps.

---
+++ aps-20250421/src/Aps.php
@@ -0,0 +1,43 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Aps\V20250421;
+
+use Darabonba\OpenApi\OpenApiClient;
+use Darabonba\OpenApi\Utils;
+
+class Aps extends OpenApiClient
+{
+    public function __construct($config)
+    {
+        parent::__construct($config);
+        $this->_endpointRule = '';
+        $this->checkConfig($config);
+        $this->_endpoint = $this->getEndpoint('aps', $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
+    }
+
+    /**
+     * @param string   $productId
+     * @param string   $regionId
+     * @param string   $endpointRule
+     * @param string   $network
+     * @param string   $suffix
+     * @param string[] $endpointMap
+     * @param string   $endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint($productId, $regionId, $endpointRule, $network, $suffix, $endpointMap, $endpoint)
+    {
+        if (null !== $endpoint) {
+            return $endpoint;
+        }
+
+        if (null !== $endpointMap && null !== @$endpointMap[$regionId]) {
+            return @$endpointMap[$regionId];
+        }
+
+        return Utils::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
+    }
+}
