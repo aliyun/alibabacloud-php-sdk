15c3d4aa4ef7b79d7a672e64a6c0a0d8626235cc
sdk-team <sdk-team@alibabacloud.com>
2025-12-24 09:19:23 +0000

    Generated php 2025-09-01 for AgentIdentity.

--- agentidentity-20250901/src/AgentIdentity.php
+++ agentidentity-20250901/src/AgentIdentity.php
@@ -61,7 +61,7 @@ class AgentIdentity extends OpenApiClient
     public function __construct($config)
     {
         parent::__construct($config);
-        $this->_endpointRule = '';
+        $this->_endpointRule = 'regional';
         $this->checkConfig($config);
         $this->_endpoint = $this->getEndpoint('agentidentity', $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
     }
