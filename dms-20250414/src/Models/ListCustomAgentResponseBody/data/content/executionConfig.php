41649f0f0eacfb15d01e9bca89782ce007e3108b
sdk-team <sdk-team@alibabacloud.com>
2026-01-07 17:36:18 +0000

    Update API DescribeCustomAgent: add response parameters Body.Data.ExecutionConfig.SkipAskHuman.

--- dms-20250414/src/Models/ListCustomAgentResponseBody/data/content/executionConfig.php
+++ dms-20250414/src/Models/ListCustomAgentResponseBody/data/content/executionConfig.php
@@ -11,9 +11,27 @@ class executionConfig extends Model
     /**
      * @var bool
      */
+    public $skipAskHuman;
+
+    /**
+     * @var bool
+     */
     public $skipPlan;
+
+    /**
+     * @var bool
+     */
+    public $skipSqlConfirm;
+
+    /**
+     * @var bool
+     */
+    public $skipWebReportConfirm;
     protected $_name = [
+        'skipAskHuman' => 'SkipAskHuman',
         'skipPlan' => 'SkipPlan',
+        'skipSqlConfirm' => 'SkipSqlConfirm',
+        'skipWebReportConfirm' => 'SkipWebReportConfirm',
     ];
 
     public function validate()
@@ -24,8 +42,20 @@ class executionConfig extends Model
     public function toArray($noStream = false)
     {
         $res = [];
+        if (null !== $this->skipAskHuman) {
+            $res['SkipAskHuman'] = $this->skipAskHuman;
+        }
+
         if (null !== $this->skipPlan) {
             $res['SkipPlan'] = $this->skipPlan;
+        }
+
+        if (null !== $this->skipSqlConfirm) {
+            $res['SkipSqlConfirm'] = $this->skipSqlConfirm;
+        }
+
+        if (null !== $this->skipWebReportConfirm) {
+            $res['SkipWebReportConfirm'] = $this->skipWebReportConfirm;
         }
 
         return $res;
@@ -39,8 +69,20 @@ class executionConfig extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['SkipAskHuman'])) {
+            $model->skipAskHuman = $map['SkipAskHuman'];
+        }
+
         if (isset($map['SkipPlan'])) {
             $model->skipPlan = $map['SkipPlan'];
+        }
+
+        if (isset($map['SkipSqlConfirm'])) {
+            $model->skipSqlConfirm = $map['SkipSqlConfirm'];
+        }
+
+        if (isset($map['SkipWebReportConfirm'])) {
+            $model->skipWebReportConfirm = $map['SkipWebReportConfirm'];
         }
 
         return $model;
