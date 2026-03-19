0ab4e949b20f2840f81203ae35fcdee376b0f50b
sdk-team <sdk-team@alibabacloud.com>
2026-03-19 17:35:40 +0000

    Update API QueryJobList: add response parameters Body.JobList.$.Output.Properties.FileMd5.

--- mts-20140618/src/Models/QueryJobListResponseBody/jobList/job/output/properties.php
+++ mts-20140618/src/Models/QueryJobListResponseBody/jobList/job/output/properties.php
@@ -29,6 +29,11 @@ class properties extends Model
     /**
      * @var string
      */
+    public $fileMd5;
+
+    /**
+     * @var string
+     */
     public $fileSize;
 
     /**
@@ -64,6 +69,7 @@ class properties extends Model
         'bitrate' => 'Bitrate',
         'duration' => 'Duration',
         'fileFormat' => 'FileFormat',
+        'fileMd5' => 'FileMd5',
         'fileSize' => 'FileSize',
         'format' => 'Format',
         'fps' => 'Fps',
@@ -100,6 +106,10 @@ class properties extends Model
 
         if (null !== $this->fileFormat) {
             $res['FileFormat'] = $this->fileFormat;
+        }
+
+        if (null !== $this->fileMd5) {
+            $res['FileMd5'] = $this->fileMd5;
         }
 
         if (null !== $this->fileSize) {
@@ -151,6 +161,10 @@ class properties extends Model
 
         if (isset($map['FileFormat'])) {
             $model->fileFormat = $map['FileFormat'];
+        }
+
+        if (isset($map['FileMd5'])) {
+            $model->fileMd5 = $map['FileMd5'];
         }
 
         if (isset($map['FileSize'])) {
