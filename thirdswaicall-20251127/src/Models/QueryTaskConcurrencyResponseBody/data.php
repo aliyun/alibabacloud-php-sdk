89267981789128da2745676857ccb976ff96743b
sdk-team <sdk-team@alibabacloud.com>
2026-05-12 07:34:16 +0000

    Update API QueryTaskConcurrency: add response parameters Body.Data.AvailableConcurrency.

--- thirdswaicall-20251127/src/Models/QueryTaskConcurrencyResponseBody/data.php
+++ thirdswaicall-20251127/src/Models/QueryTaskConcurrencyResponseBody/data.php
@@ -11,9 +11,21 @@ class data extends Model
     /**
      * @var int
      */
+    public $availableConcurrency;
+
+    /**
+     * @var int
+     */
     public $currentConcurrency;
+
+    /**
+     * @var int
+     */
+    public $maxConcurrency;
     protected $_name = [
+        'availableConcurrency' => 'AvailableConcurrency',
         'currentConcurrency' => 'CurrentConcurrency',
+        'maxConcurrency' => 'MaxConcurrency',
     ];
 
     public function validate()
@@ -24,8 +36,16 @@ class data extends Model
     public function toArray($noStream = false)
     {
         $res = [];
+        if (null !== $this->availableConcurrency) {
+            $res['AvailableConcurrency'] = $this->availableConcurrency;
+        }
+
         if (null !== $this->currentConcurrency) {
             $res['CurrentConcurrency'] = $this->currentConcurrency;
+        }
+
+        if (null !== $this->maxConcurrency) {
+            $res['MaxConcurrency'] = $this->maxConcurrency;
         }
 
         return $res;
@@ -39,8 +59,16 @@ class data extends Model
     public static function fromMap($map = [])
     {
         $model = new self();
+        if (isset($map['AvailableConcurrency'])) {
+            $model->availableConcurrency = $map['AvailableConcurrency'];
+        }
+
         if (isset($map['CurrentConcurrency'])) {
             $model->currentConcurrency = $map['CurrentConcurrency'];
+        }
+
+        if (isset($map['MaxConcurrency'])) {
+            $model->maxConcurrency = $map['MaxConcurrency'];
         }
 
         return $model;
