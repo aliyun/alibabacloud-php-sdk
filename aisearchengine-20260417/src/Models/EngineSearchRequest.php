4cc0960f6984ff919e9de7d0aadf379ca5ae8a3b
sdk-team <sdk-team@alibabacloud.com>
2026-06-16 02:41:27 +0000

    Update API EngineSearch: delete request parameters body.recall.

--- aisearchengine-20260417/src/Models/EngineSearchRequest.php
+++ aisearchengine-20260417/src/Models/EngineSearchRequest.php
@@ -6,7 +6,6 @@ namespace AlibabaCloud\SDK\AiSearchEngine\V20260417\Models;
 
 use AlibabaCloud\Dara\Model;
 use AlibabaCloud\SDK\AiSearchEngine\V20260417\Models\EngineSearchRequest\query;
-use AlibabaCloud\SDK\AiSearchEngine\V20260417\Models\EngineSearchRequest\recall;
 use AlibabaCloud\SDK\AiSearchEngine\V20260417\Models\EngineSearchRequest\user;
 
 class EngineSearchRequest extends Model
@@ -27,47 +26,26 @@ class EngineSearchRequest extends Model
     public $query;
 
     /**
-     * @var recall
-     */
-    public $recall;
-
-    /**
      * @var string
      */
     public $sessionId;
 
     /**
-     * @var string
-     */
-    public $strategyId;
-
-    /**
      * @var user
      */
     public $user;
-
-    /**
-     * @var string
-     */
-    public $version;
     protected $_name = [
         'appId' => 'appId',
         'grey' => 'grey',
         'query' => 'query',
-        'recall' => 'recall',
         'sessionId' => 'sessionId',
-        'strategyId' => 'strategyId',
         'user' => 'user',
-        'version' => 'version',
     ];
 
     public function validate()
     {
         if (null !== $this->query) {
             $this->query->validate();
-        }
-        if (null !== $this->recall) {
-            $this->recall->validate();
         }
         if (null !== $this->user) {
             $this->user->validate();
@@ -90,24 +68,12 @@ class EngineSearchRequest extends Model
             $res['query'] = null !== $this->query ? $this->query->toArray($noStream) : $this->query;
         }
 
-        if (null !== $this->recall) {
-            $res['recall'] = null !== $this->recall ? $this->recall->toArray($noStream) : $this->recall;
-        }
-
         if (null !== $this->sessionId) {
             $res['sessionId'] = $this->sessionId;
         }
 
-        if (null !== $this->strategyId) {
-            $res['strategyId'] = $this->strategyId;
-        }
-
         if (null !== $this->user) {
             $res['user'] = null !== $this->user ? $this->user->toArray($noStream) : $this->user;
-        }
-
-        if (null !== $this->version) {
-            $res['version'] = $this->version;
         }
 
         return $res;
@@ -133,24 +99,12 @@ class EngineSearchRequest extends Model
             $model->query = query::fromMap($map['query']);
         }
 
-        if (isset($map['recall'])) {
-            $model->recall = recall::fromMap($map['recall']);
-        }
-
         if (isset($map['sessionId'])) {
             $model->sessionId = $map['sessionId'];
         }
 
-        if (isset($map['strategyId'])) {
-            $model->strategyId = $map['strategyId'];
-        }
-
         if (isset($map['user'])) {
             $model->user = user::fromMap($map['user']);
-        }
-
-        if (isset($map['version'])) {
-            $model->version = $map['version'];
         }
 
         return $model;
