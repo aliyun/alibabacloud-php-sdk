129e68f2742ac27facaeb9ffaa66b8ee691cf5f6
sdk-team <sdk-team@alibabacloud.com>
2026-07-07 09:59:51 +0000

    Generated php 2022-03-02 for Green.

--- green-20220302/src/Green.php
+++ green-20220302/src/Green.php
@@ -1817,7 +1817,7 @@ class Green extends OpenApiClient
      * Retrieves the task result of an enhanced video content moderation node.
      *
      * @remarks
-     * This operation is not billed. Set the query interval to 30 seconds (that is, query the results 30 seconds after submitting the asynchronous moderation task). The maximum query window is 24 hours. After 24 hours, the results are automatically deleted.
+     * This operation is not billed. Set the polling interval to 30 seconds (query results 30 seconds after submitting the asynchronous moderation task). The maximum query window is 24 hours. After that, results are automatically deleted.
      *
      * @param request - VideoModerationResultRequest
      * @param runtime - runtime options for this request RuntimeOptions
@@ -1863,7 +1863,7 @@ class Green extends OpenApiClient
      * Retrieves the task result of an enhanced video content moderation node.
      *
      * @remarks
-     * This operation is not billed. Set the query interval to 30 seconds (that is, query the results 30 seconds after submitting the asynchronous moderation task). The maximum query window is 24 hours. After 24 hours, the results are automatically deleted.
+     * This operation is not billed. Set the polling interval to 30 seconds (query results 30 seconds after submitting the asynchronous moderation task). The maximum query window is 24 hours. After that, results are automatically deleted.
      *
      * @param request - VideoModerationResultRequest
      *
