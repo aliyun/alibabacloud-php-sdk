fde49f45099c03af95dbbf7079ba3af1d7a58df8
sdk-team <sdk-team@alibabacloud.com>
2026-07-08 02:01:21 +0000

    Support API VirtualThreeElementsVerification.

---
+++ dytnsapi-20200217/src/Models/VirtualThreeElementsVerificationRequest.php
@@ -0,0 +1,104 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Dytnsapi\V20200217\Models;
+
+use AlibabaCloud\Dara\Model;
+
+class VirtualThreeElementsVerificationRequest extends Model
+{
+    /**
+     * @var string
+     */
+    public $authCode;
+
+    /**
+     * @var string
+     */
+    public $certCode;
+
+    /**
+     * @var string
+     */
+    public $certName;
+
+    /**
+     * @var string
+     */
+    public $inputNumber;
+
+    /**
+     * @var string
+     */
+    public $mask;
+    protected $_name = [
+        'authCode' => 'AuthCode',
+        'certCode' => 'CertCode',
+        'certName' => 'CertName',
+        'inputNumber' => 'InputNumber',
+        'mask' => 'Mask',
+    ];
+
+    public function validate()
+    {
+        parent::validate();
+    }
+
+    public function toArray($noStream = false)
+    {
+        $res = [];
+        if (null !== $this->authCode) {
+            $res['AuthCode'] = $this->authCode;
+        }
+
+        if (null !== $this->certCode) {
+            $res['CertCode'] = $this->certCode;
+        }
+
+        if (null !== $this->certName) {
+            $res['CertName'] = $this->certName;
+        }
+
+        if (null !== $this->inputNumber) {
+            $res['InputNumber'] = $this->inputNumber;
+        }
+
+        if (null !== $this->mask) {
+            $res['Mask'] = $this->mask;
+        }
+
+        return $res;
+    }
+
+    public function toMap($noStream = false)
+    {
+        return $this->toArray($noStream);
+    }
+
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['AuthCode'])) {
+            $model->authCode = $map['AuthCode'];
+        }
+
+        if (isset($map['CertCode'])) {
+            $model->certCode = $map['CertCode'];
+        }
+
+        if (isset($map['CertName'])) {
+            $model->certName = $map['CertName'];
+        }
+
+        if (isset($map['InputNumber'])) {
+            $model->inputNumber = $map['InputNumber'];
+        }
+
+        if (isset($map['Mask'])) {
+            $model->mask = $map['Mask'];
+        }
+
+        return $model;
+    }
+}
